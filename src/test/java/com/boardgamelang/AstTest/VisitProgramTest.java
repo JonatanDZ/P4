91728ac88de037b922da37f1a44439d61b2e784f
Nicolai-Schultz <nicoschultz42@gmail.com>
2026-04-21 14:23:30 +0200

    tests work

--- src/test/java/com/boardgamelang/AstTest/VisitProgramTest.java
+++ src/test/java/com/boardgamelang/AstTest/VisitProgramTest.java
@@ -3,6 +3,8 @@ package com.boardgamelang.AstTest;
 import com.boardgamelang.AST.AstBuilder;
 import com.boardgamelang.AST.def.BoardNode;
 import com.boardgamelang.AST.program.ProgramNode;
+import com.boardgamelang.AST.stmt.AssertNode;
+import com.boardgamelang.AST.stmt.StmtNode;
 import com.boardgamelang.BoardGameLangParser;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +27,9 @@ public class VisitProgramTest {
         BoardNode board = (BoardNode) node.defNodes.get(0);
         assertEquals(3, board.width);
         assertEquals(3, board.height);
+
+        AssertNode stmt = (AssertNode) node.stmtNodes.get(0);
+        assertEquals("foo", stmt.ident);
+
     }
 }
