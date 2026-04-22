6c75098a8e83a5982472121f795acfa1e5eac771
Cecilie Lassen <classe23@student.aau.dk>
2026-04-22 14:46:47 +0200

    Create VisitBoardNodeTest.java
    Test for BoardNode er blevet lavet

---
+++ src/test/java/com/boardgamelang/AstTest/VisitBoardNodeTest.java
@@ -0,0 +1,22 @@
+package com.boardgamelang.AstTest;
+
+import com.boardgamelang.AST.AstBuilder;
+import com.boardgamelang.AST.def.BoardNode;
+import com.boardgamelang.BoardGameLangParser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VisitBoardNodeTest {
+    @Test
+    void visitBoardNode() {
+        String input = "(3,3)";
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        BoardNode node = (BoardNode) builder.visit(parser.def());
+
+        assertEquals(3, node.width);
+        assertEquals(3, node.height);
+    }
+}
