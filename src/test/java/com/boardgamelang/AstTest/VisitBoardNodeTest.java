c429b5e63bae8fe1eab038315e42b8c527aff196
Nikolaidk3 <hp82fk@student.aau.dk>
2026-04-27 12:40:53 +0200

    updated boradNode test

--- src/test/java/com/boardgamelang/AstTest/VisitBoardNodeTest.java
+++ src/test/java/com/boardgamelang/AstTest/VisitBoardNodeTest.java
@@ -2,6 +2,7 @@ package com.boardgamelang.AstTest;
 
 import com.boardgamelang.AST.AstBuilder;
 import com.boardgamelang.AST.def.BoardNode;
+import com.boardgamelang.AST.program.ProgramNode;
 import com.boardgamelang.BoardGameLangParser;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VisitBoardNodeTest {
     @Test
     void visitBoardNode() {
-        String input = "(3,3);";
+        String input = "board(3,3);";
         BoardGameLangParser parser = ParseTreeHelper.createParser(input);
         AstBuilder builder = new AstBuilder();
 
-        BoardNode node = (BoardNode) builder.visit(parser.def());
+        BoardNode node = (BoardNode) builder.visit(parser.program());
 
         assertEquals(3, node.pos.x);
         assertEquals(3, node.pos.y);
