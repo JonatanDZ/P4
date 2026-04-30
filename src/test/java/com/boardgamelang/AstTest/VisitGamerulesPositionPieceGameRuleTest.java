e757f1096e2bd118385f7b2bc7eab60cb04fd1df
Cecilie Lassen <classe23@student.aau.dk>
2026-04-30 11:19:09 +0200

    More tests

--- src/test/java/com/boardgamelang/AstTest/VisitGamerulesPositionPieceGameRuleTest.java
+++ src/test/java/com/boardgamelang/AstTest/VisitGamerulesPositionPieceGameRuleTest.java
@@ -2,12 +2,10 @@ package com.boardgamelang.AstTest;
 
 import com.boardgamelang.AST.AstBuilder;
 import com.boardgamelang.AST.bexp.BexpNode;
-import com.boardgamelang.AST.bexp.OccupiedNode;
 import com.boardgamelang.AST.gamerule.GamerulesPositionPieceNode;
 import com.boardgamelang.BoardGameLangParser;
 import org.antlr.v4.runtime.*;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VisitGamerulesPositionPieceGameRuleTest {
@@ -15,6 +13,17 @@ public class VisitGamerulesPositionPieceGameRuleTest {
     // Test to check the entered value is a bexp
     @Test
     void visitGamerulesExpectsBexp() {
+        String input = "gamerules position piece {occupied(2,5)};";
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        GamerulesPositionPieceNode node = (GamerulesPositionPieceNode) builder.visit(parser.gameRule());
+
+        assertInstanceOf(BexpNode.class, node.bexp);
+    }
+    // Test to check the entered value is a bexp
+    @Test
+    void visitGamerulesExpectsOccupiedAndOccupied() {
         String input = "gamerules position piece {occupied(2,5) and occupied(3,5)};";
         BoardGameLangParser parser = ParseTreeHelper.createParser(input);
         AstBuilder builder = new AstBuilder();
@@ -24,4 +33,25 @@ public class VisitGamerulesPositionPieceGameRuleTest {
         assertInstanceOf(BexpNode.class, node.bexp);
     }
 
+    @Test
+    void visitGamerulesExpectsOccupiedOrOccupied() {
+        String input = "gamerules position piece {occupied(1,7) or occupied(4,9)};";
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        GamerulesPositionPieceNode node = (GamerulesPositionPieceNode) builder.visit(parser.gameRule());
+
+        assertInstanceOf(BexpNode.class, node.bexp);
+    }
+
+    @Test
+    void visitGamerulesExpectsOccupiedAndOccupiedOrOccupied() {
+        String input = "gamerules position piece {occupied(1,7) and occupied(3,7) or occupied(4,9)};";
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        GamerulesPositionPieceNode node = (GamerulesPositionPieceNode) builder.visit(parser.gameRule());
+
+        assertInstanceOf(BexpNode.class, node.bexp);
+    }
 }
--- src/test/java/com/boardgamelang/InterpreterTest/InterpretGamerulesPositionPieceTest.java
+++ src/test/java/com/boardgamelang/InterpreterTest/InterpretGamerulesPositionPieceTest.java
@@ -2,20 +2,18 @@ package com.boardgamelang.InterpreterTest;
 
 import com.boardgamelang.AST.AstBuilder;
 import com.boardgamelang.AST.bexp.BexpNode;
-import com.boardgamelang.AST.bexp.OccupiedNode;
 import com.boardgamelang.AST.program.ProgramNode;
 import com.boardgamelang.AstTest.ParseTreeHelper;
 import com.boardgamelang.BoardGameLangParser;
 import com.boardgamelang.interpreter.Interpreter;
 import com.boardgamelang.interpreter.State;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class InterpretGamerulesPositionPieceTest {
+
     @Test
-    void interpretGamerulesPositionPieceSetg() {
+    void interpretGamerulesPositionPieceBexp() {
         String input = "board(3,3); gamerules position piece {occupied(3,2)};";
 
         BoardGameLangParser parser = ParseTreeHelper.createParser(input);
@@ -26,4 +24,43 @@ public class InterpretGamerulesPositionPieceTest {
 
         assertInstanceOf(BexpNode.class, state.g);
     }
+
+    @Test
+    void visitGamerulesExpectsOccupiedAndOccupied() {
+        String input = "board(7,9); gamerules position piece {occupied(2,5) and occupied(3,5)};";
+
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        ProgramNode programNode = (ProgramNode) builder.visit(parser.program());
+        State state = new Interpreter().run(programNode);
+
+        assertInstanceOf(BexpNode.class, state.g);
+    }
+
+    @Test
+    void visitGamerulesExpectsOccupiedOrOccupied() {
+        String input = "board(9,12); gamerules position piece {occupied(1,7) or occupied(4,9)};";
+
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        ProgramNode programNode = (ProgramNode) builder.visit(parser.program());
+        State state = new Interpreter().run(programNode);
+
+        assertInstanceOf(BexpNode.class, state.g);
+    }
+
+    @Test
+    void visitGamerulesExpectsOccupiedAndOccupiedOrOccupied() {
+        String input = "board(13,10); gamerules position piece {occupied(1,7) and occupied(3,7) or occupied(4,9)};";
+        BoardGameLangParser parser = ParseTreeHelper.createParser(input);
+        AstBuilder builder = new AstBuilder();
+
+        ProgramNode programNode = (ProgramNode) builder.visit(parser.program());
+        State state = new Interpreter().run(programNode);
+
+        assertInstanceOf(BexpNode.class, state.g);
+    }
+
 }
