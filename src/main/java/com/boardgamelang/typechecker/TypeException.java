dcc1a13eff67649cd5ba53394481ac0e41596add
Nikolaidk3 <hp82fk@student.aau.dk>
2026-05-04 12:49:13 +0200

    comment

--- src/main/java/com/boardgamelang/typechecker/TypeException.java
+++ src/main/java/com/boardgamelang/typechecker/TypeException.java
@@ -1,6 +1,7 @@
 package com.boardgamelang.typechecker;
 
 public class TypeException extends RuntimeException {
+    // This is for convenience in tests, that the error specifically comes from the type checker. 
     public TypeException(String message) {
         super(message);
     }
