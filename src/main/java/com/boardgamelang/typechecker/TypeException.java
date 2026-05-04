1a314558e179285f2771c911c6111f7b472bcbb9
Nicolai Schultz <130221724+Nicolai-Schultz@users.noreply.github.com>
2026-05-04 12:52:24 +0200

    Update comments in TypeException class
    Clarify purpose of TypeException class in comments.

--- src/main/java/com/boardgamelang/typechecker/TypeException.java
+++ src/main/java/com/boardgamelang/typechecker/TypeException.java
@@ -1,7 +1,8 @@
 package com.boardgamelang.typechecker;
 
 public class TypeException extends RuntimeException {
-    // This is for convenience in tests, that the error specifically comes from the type checker. 
+    // This is an alias for RuntimeException. It is not strictly necessary. TypeException does not differ from RuntimeException semantically.
+    // It adds readability in code by separating RuntimeExceptions and TypeExceptions (exceptions at compile time).
     public TypeException(String message) {
         super(message);
     }
